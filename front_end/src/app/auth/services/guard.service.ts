0540e7c03d089753e1a87176af7ebfeae0aecbb4
hseinhmede <hseinhmede123@gmail.com>
2020-01-02 14:16:06 +0100

    modifier les auth

--- front_end/src/app/auth/services/guard.service.ts
+++ front_end/src/app/auth/services/guard.service.ts
@@ -6,7 +6,7 @@ import {AuthService} from './auth.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class Guard implements CanActivate {
 
   constructor(private  authService: AuthService, private  route: Router) {
   }
